1a81382809c389d0697574e633af789e9b6815e7
s9848261712 <122036718+s9848261712@users.noreply.github.com>
2023-01-13 15:45:43 +0000

    git commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# rentalcarsv1.2
---
+++ src/main/resources/mainController.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class mainController {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scan = new scanner(system.in)
+				
+				System.out.println("enter number");
+		int num = scan.nextInt()
+				System.out.println("entered number is" +num);
+		
+	}
+
+}
